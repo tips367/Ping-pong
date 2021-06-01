30ce1d379020287c3a2b40e9cce8497e97298441
khantipu <Tipu.Khan@landisgyr.com>
2021-06-01 22:58:33 +0530

    Enhancing by using Bulma

--- app.js
+++ app.js
@@ -1,46 +1,53 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#playTo");
-
-let p1Score = 0;
-let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener("click", () => {
+function updateScores(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    p1Display.textContent = p1Score;
+    player.display.textContent = player.score;
   }
-});
+}
 
-p2Button.addEventListener("click", () => {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-    }
-    p2Display.textContent = p2Score;
-  }
+p1.button.addEventListener("click", () => {
+  updateScores(p1, p2);
+});
+p2.button.addEventListener("click", () => {
+  updateScores(p2, p1);
 });
 
 resetButton.addEventListener("click", reset);
 
-winningScoreSelect.addEventListener("change", function(){
+winningScoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
   reset();
 });
 
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove("has-text-success", "has-text-danger");
+    p.button.disabled = false;
+  }
 }
